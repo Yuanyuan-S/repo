0290b797bf70cb596176dff3e7d6cccba93f8b1b
Yuanyuan Shou <yuanyuan.shou@edu.turkuamk.fi>
2020-09-04 13:44:23 +0300

    little change

---
+++ 1.py
@@ -0,0 +1,7 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("I am a beginner with Git")
+I am a beginner with Git
+>>> print(1+2)
+3
+>>> 
